05ad22361837916b35af87ead34ec3dc6cb3f20d
Ling Yeou Yi <enily_ling@hotmail.com>
2019-11-11 11:25:13 +0800

    initial

---
+++ app/src/main/java/com/example/bmicalculator_excersice2/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.bmicalculator_excersice2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener{
+            CalculateBMI()
+        }
+
+        buttonReset.setOnClickListener{
+            Reset()
+        }
+    }
+
+    private fun CalculateBMI(){
+        if(editTextWeight.text.isEmpty()){
+            editTextWeight.setError(getString(R.string.input_error))
+            return
+        }
+        if(editTextHeight.text.isEmpty()){
+            editTextHeight.setError(getString(R.string.input_error))
+        }
+
+        val weight = editTextWeight.text.toString().toFloat()
+        val height = editTextHeight.text.toString().toFloat()
+        val bmi = weight / (height/100).pow(2)
+
+        if(bmi < 18.5){
+            //textViewBMI.setText(getString(R.string.bmi) + ...)
+            textViewBMI.text = String.format("%s %.2f %s", getString(R.string.bmi), bmi, getString(R.string.under))
+            imageViewProfile.setImageResource(R.drawable.under)
+        }
+        else if(bmi > 25){
+            textViewBMI.text = String.format("%s %.2f %s", getString(R.string.bmi), bmi, getString(R.string.over))
+            imageViewProfile.setImageResource(R.drawable.over)
+        }
+        else{
+            textViewBMI.text = String.format("%s %.2f %s", getString(R.string.bmi), bmi, getString(R.string.normal))
+            imageViewProfile.setImageResource(R.drawable.normal)
+        }
+    }
+
+    private fun Reset(){
+        editTextWeight.setText("")
+        editTextHeight.setText("")
+        textViewBMI.setText("")
+        imageViewProfile.setImageResource(R.drawable.empty)
+    }
+}
